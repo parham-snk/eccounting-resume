2cfaf3dc62528da558ecdf27dc09b3ee1a879088
parham-snk <psanjanaki@gmail.com>
2026-02-07 12:42:07 +0330

    debug category drag&drop;

--- src/pages/cat/li.jsx
+++ src/pages/cat/li.jsx
@@ -5,7 +5,7 @@ import { FaWindowClose } from "react-icons/fa";
 import { Context } from "../../context/Context";
 const Li = props => {
     const { cat, children } = props?.item
-    const { removeCat,changeParent } = useContext(Context)
+    const { removeCat, changeParent } = useContext(Context)
     const [ul, setUl] = useState()
     useEffect(() => {
         $(document).ready(function () {
@@ -22,13 +22,15 @@ const Li = props => {
         setUl(elements)
     }, [])
     return (
-        <li id={`li-${cat.cat_id}`} >
+        <li id={`li-${cat.cat_id}`} onDragOver={ev => console.log(ev)}>
             <p className={`flex flex-row justify-between items-center`} draggable="true" onDragOver={e => e.preventDefault()} onDragStart={(ev) => {
                 ev.dataTransfer.setData("li", `li-${cat.cat_id}`)
             }} onDrop={ev => {
                 ev.preventDefault()
                 let id = ev.dataTransfer.getData("li").split("-")[1]
-                changeParent({cat_id:id,parent_id:cat.cat_id})
+                if (id != cat.cat_id) {
+                    changeParent({ cat_id: id, parent_id: cat.cat_id })
+                }
                 // ev.target.appendChild(document.getElementById(id).parentElement)
             }}>
                 <p id={cat.cat_id} className={`${[...children].length > 0 ? "cursor-pointer" : null}`}>
