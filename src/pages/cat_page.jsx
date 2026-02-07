19b04c15442332a1219f1ebd34fb1947cc90872d
parham-snk <psanjanaki@gmail.com>
2026-02-07 21:04:03 +0330

    add drag&drop for first parent

--- src/pages/cat_page.jsx
+++ src/pages/cat_page.jsx
@@ -9,10 +9,11 @@ function hash(str) {
 }
 const CAT_PAGE = (props) => {
     // const { updateList } = props
-    let { cats } = useContext(Context)
+    let { cats, changeParent } = useContext(Context)
     const [catList, setCatList] = useState(cats)
     const [key, setkey] = useState(1)
     const [elements, setElements] = useState([])
+    const [hover, setHover] = useState(false)
     useEffect(() => {
         const randomNumber = Math.floor(Math.random().toFixed(2))
         setkey(randomNumber)
@@ -27,7 +28,32 @@ const CAT_PAGE = (props) => {
             </div>
             <div className="w-full overflow-y-scroll z-0 py-9">
 
-                <div className="flex flex-row flex-wrap w-full h-fit cats">
+                <div className={`${hover ? "bg-blue-300" : "bg-gray-100"} flex flex-row flex-wrap w-full h-fit cats relative`}
+
+                >
+                    <div className="absolute w-full h-full top-0 left-0"
+
+                        onDragOver={e => {
+                            e.preventDefault()
+                            setHover(true)
+                        }}
+                        onDragLeave={() => {
+                            setHover(false)
+                        }}
+                        onDrop={ev => {
+                            ev.preventDefault()
+                            let id = ev.dataTransfer.getData("li").split("-")[1]
+
+                            changeParent({ cat_id: id, parent_id: 100 })
+
+                            setHover(false)
+                            // ev.target.appendChild(document.getElementById(id).parentElement)
+                        }}
+                    >
+                        {
+                            //virtual element
+                        }
+                    </div>
                     {
                         catList && catList.map((item, index) => {
                             return < CatField key={hash(item)} cat={item} />
